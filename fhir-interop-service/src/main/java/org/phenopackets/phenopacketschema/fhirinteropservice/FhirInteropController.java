aa816cc42b9c2acc3d5cb01cbcc93759c1029d63
Jules Jacobsen <j.jacobsen@qmul.ac.uk>
2018-09-26 15:55:05 +0100

    Allow cross origin requests on FhirInteropController

--- fhir-interop-service/src/main/java/org/phenopackets/phenopacketschema/fhirinteropservice/FhirInteropController.java
+++ fhir-interop-service/src/main/java/org/phenopackets/phenopacketschema/fhirinteropservice/FhirInteropController.java
@@ -8,6 +8,7 @@ import org.phenopackets.schema.v1.fhir.interop.converters.FhirConverter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,6 +27,7 @@ public class FhirInteropController {
         this.jsonParser = FhirContext.forDstu3().newJsonParser();
     }
 
+    @CrossOrigin
     @PostMapping(value = "convert",
             consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,
             produces = MediaType.APPLICATION_JSON_UTF8_VALUE
